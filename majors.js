91ed1ba0136bb40f91e5cb4bd65e7d31d17b9733
Ban Cheah <Ban Cheah>
2016-12-12 14:34:15 -0500

    Added radio buttons, US map
    Radio buttons and styles added. US map added.

--- majors.js
+++ majors.js
@@ -29,6 +29,42 @@ function hexToRGB(hex, alpha){
   return "rgba(" + r + ", " + g + ", " + b + a + ")";
 }
 
+function convert_state(name, to) {
+    var states = new Array(                         {'name':'Alabama', 'abbrev':'AL'},          {'name':'Alaska', 'abbrev':'AK'},
+        {'name':'Arizona', 'abbrev':'AZ'},          {'name':'Arkansas', 'abbrev':'AR'},         {'name':'California', 'abbrev':'CA'},
+        {'name':'Colorado', 'abbrev':'CO'},         {'name':'Connecticut', 'abbrev':'CT'},      {'name':'Delaware', 'abbrev':'DE'},
+        {'name':'Florida', 'abbrev':'FL'},          {'name':'Georgia', 'abbrev':'GA'},          {'name':'Hawaii', 'abbrev':'HI'},
+        {'name':'Idaho', 'abbrev':'ID'},            {'name':'Illinois', 'abbrev':'IL'},         {'name':'Indiana', 'abbrev':'IN'},
+        {'name':'Iowa', 'abbrev':'IA'},             {'name':'Kansas', 'abbrev':'KS'},           {'name':'Kentucky', 'abbrev':'KY'},
+        {'name':'Louisiana', 'abbrev':'LA'},        {'name':'Maine', 'abbrev':'ME'},            {'name':'Maryland', 'abbrev':'MD'},
+        {'name':'Massachusetts', 'abbrev':'MA'},    {'name':'Michigan', 'abbrev':'MI'},         {'name':'Minnesota', 'abbrev':'MN'},
+        {'name':'Mississippi', 'abbrev':'MS'},      {'name':'Missouri', 'abbrev':'MO'},         {'name':'Montana', 'abbrev':'MT'},
+        {'name':'Nebraska', 'abbrev':'NE'},         {'name':'Nevada', 'abbrev':'NV'},           {'name':'New Hampshire', 'abbrev':'NH'},
+        {'name':'New Jersey', 'abbrev':'NJ'},       {'name':'New Mexico', 'abbrev':'NM'},       {'name':'New York', 'abbrev':'NY'},
+        {'name':'North Carolina', 'abbrev':'NC'},   {'name':'North Dakota', 'abbrev':'ND'},     {'name':'Ohio', 'abbrev':'OH'},
+        {'name':'Oklahoma', 'abbrev':'OK'},         {'name':'Oregon', 'abbrev':'OR'},           {'name':'Pennsylvania', 'abbrev':'PA'},
+        {'name':'Rhode Island', 'abbrev':'RI'},     {'name':'South Carolina', 'abbrev':'SC'},   {'name':'South Dakota', 'abbrev':'SD'},
+        {'name':'Tennessee', 'abbrev':'TN'},        {'name':'Texas', 'abbrev':'TX'},            {'name':'Utah', 'abbrev':'UT'},
+        {'name':'Vermont', 'abbrev':'VT'},          {'name':'Virginia', 'abbrev':'VA'},         {'name':'Washington', 'abbrev':'WA'},
+        {'name':'West Virginia', 'abbrev':'WV'},    {'name':'Wisconsin', 'abbrev':'WI'},        {'name':'Wyoming', 'abbrev':'WY'},
+        {'name':'District of Columbia', 'abbrev':'DC'}
+        );
+    $.each(states, function(index, value){
+        if (to == 'name') {
+            if (value.abbrev.toLowerCase() == name.toLowerCase()){
+                returnthis = value.name;
+                return false;
+            }
+        } else if (to == 'abbrev') {
+            if (value.name.toLowerCase() == name.toLowerCase()){
+                returnthis = value.abbrev.toUpperCase();
+                return false;
+            }
+        }
+    });
+    return returnthis;
+}
+
 function change_major(pmajorname, pbadata, pgraddata, balines, gradlines) {
     // Get the modal
     var str_gmajor = pmajorname;
@@ -662,6 +698,23 @@ $(function () {
     });
     $('#social-sciences-icon').click(function(){
       change_major('Social sciences', arr_badmajor, arr_graddmajor, overall_ba, overall_grad);
+    });
+
+    $('#map').usmap({
+    showLabels: true,
+    stateHoverStyles: {fill: '#DABA61'},
+    
+    'mouseover': function(event, data) {
+      var FullState = convert_state(data.name,"name");    
+      $('#clicked-state').text('Click for selection to take effect: '+ FullState);
+    },
+    'mouseout': function(event, data) {
+      $('#clicked-state').text('Select a state below.');
+    },      
+    'click' : function(event, data) {
+      var FullState = convert_state(data.name,"name");    
+      $('#selected-state').html('You have selected: ' + FullState + '. Now select a major on the left.');
+      }
     });
     
   });
